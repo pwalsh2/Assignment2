e7ecd9487bb92eb9df98689884453f6df4033672
pwalsh2 <pwalsh2@unb.ca>
2022-02-15 22:51:57 -0400

    comemfds

--- spellcorrect_starter.py
+++ spellcorrect_starter.py
@@ -1,6 +1,6 @@
 import math
 import string
-
+count = 0
 eps = 0.0001
 class UnsmoothedUnigramLM:
     def __init__(self, fname):
@@ -193,11 +193,14 @@ class SmoothedBigramLM:
         bigram = prior + " " + target_word
         if bigram in self.freqs and prior in self.uni_gram_lm.freqs:
             return math.log(self.freqs[bigram] + 1) - math.log(self.uni_gram_lm.freqs[prior] + len(self.uni_gram_lm.freqs))
+        
+        elif(prior in self.uni_gram_lm.freqs):
+            return math.log(1) - math.log(self.uni_gram_lm.freqs[prior] + len(self.uni_gram_lm.freqs))
         else:
-
             # This is a bit of a hack to get a float with the value of
             # minus 
             # infinity for words that have probability 0
+            count += 1
             return 0
 
     def in_vocab(self, word):
@@ -308,14 +311,13 @@ if __name__ == '__main__':
             ivc_log_probB = lm.log_prob(next_word, ivc)
           
             ivc_log_prob = ivc_log_probA + ivc_log_probB
-            # print(ivc_log_prob)
-            # print(ivc)
+           
             if(ivc_log_prob < 0 ):
-                ivc_log_prob = ivc_log_prob * -1
+                ivc_log_prob = ivc_log_prob * - 1
             if ivc_log_prob > best_prob:
                 best_prob = ivc_log_prob
                 best_correction = ivc
         
         print(best_correction)
 
-
+   
